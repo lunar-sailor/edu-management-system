741d0784c8e524d01cd3178131c6b81f458777e6
lunar-sailor <lunar-sailor@qq.com>
2025-04-05 12:00:55 +0800

    Initial Commit

---
+++ src/main/java/com/example/edumanagementsystem/EduManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.edumanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/edumanagementsystem/EduManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.edumanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
